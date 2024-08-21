026e87f542b03e22bb2532cbef8139f78d50e70d
minuyuhansi <muthumunigesilva21@gmail.com>
2024-08-21 21:35:54 +0530

    Add files via upload

---
+++ rockPaperScissors-Game/script.js
@@ -0,0 +1,8 @@
+document.getElementById('settingsIcon').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'flex';
+  });
+  
+  document.getElementById('closePopup').addEventListener('click', function() {
+    document.getElementById('popup').style.display = 'none';
+  });
+  
